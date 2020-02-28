221aa42bb47d7db89c68939e4c9133426f0a4a4a
Mihaela Atanasova <ma766@york.ac.uk>
2020-02-28 15:10:21 +0000

    Improved sugar scoring

--- sails-lib.cpp
+++ sails-lib.cpp
@@ -208,9 +208,7 @@ clipper::MMonomer sails::get_ideal_monomer ( const sails::data::fingerprint& fp 
     privateer::restraints::CarbohydrateDictionary dict;
     dict.read_from_monlib ( fp.name_short );
 
-    for ( int index = 0; index < fp.num_control_points ; index++ )//hardcoded for NAG; needs to be changed with output from the fingerprinting tool
-
-    //for ( int index = 0; index < fp.num_control_points ; index++ )
+    for ( int index = 0; index < fp.num_control_points ; index++ )
     {
         clipper::MAtom tmp_atm;      
         clipper::Coord_orth coords(fp.atoms[index].x, fp.atoms[index].y, fp.atoms[index].z );
@@ -458,11 +456,17 @@ clipper::MiniMol sails::build_sugars ( clipper::Xmap<float>& xwrk, sails::data::
         std::reverse( results.begin(), results.end() );
         std::cout << results.size() << std::endl;
 
-        for ( int j = 0; j < clipper::Util::min( int(results.size()), nhit ); j++ )
+
+        for ( int j = 0; j < int(results.size()); j++ )
+        //for ( int j = 0; j < clipper::Util::min( int(results.size()), nhit ); j++ )
             std::cout << j << " " << results[j].score << " " << results[j].rot << " " << results[j].trn << std::endl;
         
+        // initialise vector of placed coorinates
+        std::vector<clipper::Coord_orth> placed_coordinates; 
 
-        for ( int k = 0; k < clipper::Util::min( int(results.size()), nhit ); k++ )
+        for ( int k = 0; k <  int(results.size()); k++ )
+
+        //for ( int k = 0; k < clipper::Util::min( int(results.size()), nhit ); k++ )
         {
         clipper::String id; int roff;
 
@@ -480,40 +484,78 @@ clipper::MiniMol sails::build_sugars ( clipper::Xmap<float>& xwrk, sails::data::
         clipper::MPolymer mprot = mprep;
         int ir = results[k].rot;
         int it = results[k].trn;
+        // Get coord_orth
+        clipper::Coord_orth coord_result = xwrk.coord_orth(grid.deindex(it).coord_map());
+        double cutoff=0;
+        std::vector<double> distance_vector;
+
+        if (k == 0){
+            placed_coordinates.push_back(coord_result);
+        }
+        //std::cout << "coord_result:" << coord_result.x() << " " << coord_result.y() << " " << coord_result.z() << std::endl;
+        else
+        {
+            // If it's close to any in placed coord vector then continue the results loop        
+            for (int s = 0; s < int(placed_coordinates.size()); s++)
+            {
+            double distance = (coord_result - placed_coordinates[s]).lengthsq();
+            distance_vector.push_back(distance);
+            }
+            if (std::any_of( distance_vector.begin(), distance_vector.end(), [] (int t){return t<0.1;}) )
+            break;
+            else
+            // If not add it to the placed coord vector and do all stuff below
+            {
+                placed_coordinates.push_back(coord_result);
+            }
+        }
+
         clipper::RTop_orth rtop( rots[ir].rot(),
-                                 xwrk.coord_orth( grid.deindex(it).coord_map() ) );
+                placed_coordinates[k]); 
+    
         mprot.transform( rtop );
         mprot.set_id( id );
-
+        
         for ( int r = 0; r < mprot.size(); r++ )
             mprot[r].set_seqnum( roff+mprot[r].seqnum() );
-
+        
         mol_new.insert( mprot );
-        }
+        }                  
+        
+        // clipper::RTop_orth rtop( rots[ir].rot(),
+        //                          xwrk.coord_orth( grid.deindex(it).coord_map() ) ); // Move this coord_orth outside the rtop initialiser
+        // mprot.transform( rtop );
+        // mprot.set_id( id );
+
+        // for ( int r = 0; r < mprot.size(); r++ )
+        //     mprot[r].set_seqnum( roff+mprot[r].seqnum() );
+
+        // mol_new.insert( mprot );
+        // }
     }
     return mol_new;
 }
 
 void sails::initialise_fingerprints () {
   sails::data::fingerprint_list = {    
-    // {
-    //   "ARA", "ligand", 10, 10, 10,
-    //   { { "AO5", 2.826, 0.443, 0.013 }, { "AC5", 2.182, 0.000,-1.193 }, { "AC4", 0.772, 0.418,-1.248 },
-    //     { "AO4", 0.631, 1.605,-1.628 }, { "AC1", 2.206, 0.000, 1.183 }, { "AO1", 2.875, 0.588, 2.298 },
-    //     { "AC2", 0.731, 0.512, 1.249 }, { "AO2", 0.131, 0.300, 2.188 }, { "AC3", 0.000, 0.000, 0.000 }, { "AO3",-1.125, 0.610, 0.003 } } ,
-    //   { { "V01", 2.758, 2.629, 3.794 }, { "V02", 2.490, 5.743,-5.634 }, { "V03", 0.870, 1.227, 5.015 },
-    //     { "V04",-5.376, 2.851, 3.476 }, { "V05",-3.243, 3.161, 4.321 }, { "V06",-1.485,-2.860, 2.153 },
-    //     { "V07",-3.001,-0.086,-6.081 }, { "V08", 2.412,-2.704,-0.215 }, { "V09", 3.461, 0.901,-3.155 }, { "V10",-0.204, 4.505,-2.072 } } ,
-    //   { { "C1",  2.178, 0.000, 1.206 }, { "C2",  0.733, 0.508, 1.236 }, { "C3",  0.000, 0.000, 0.000 },
-    //     { "C4",  0.728, 0.462,-1.271 }, { "C5",  2.172, 0.000,-1.203 }, { "O1",  2.874, 0.596, 2.298 },
-    //     { "O2",  0.088, 0.115, 2.439 }, { "O3", -1.357, 0.457,-0.031 }, { "O4",  0.635, 1.868,-1.478 }, { "O5", 2.817, 0.460, 0.003 } }
-    // },
+    {
+      "ARA", "ligand", 10, 10, 10,
+      { { "AO5", 2.826, 0.443, 0.013 }, { "AC5", 2.182, 0.000,-1.193 }, { "AC4", 0.772, 0.418,-1.248 },
+        { "AO4", 0.631, 1.605,-1.628 }, { "AC1", 2.206, 0.000, 1.183 }, { "AO1", 2.875, 0.588, 2.298 },
+        { "AC2", 0.731, 0.512, 1.249 }, { "AO2", 0.131, 0.300, 2.188 }, { "AC3", 0.000, 0.000, 0.000 }, { "AO3",-1.125, 0.610, 0.003 } } ,
+      { { "V01", 2.758, 2.629, 3.794 }, { "V02", 2.490, 5.743,-5.634 }, { "V03", 0.870, 1.227, 5.015 },
+        { "V04",-5.376, 2.851, 3.476 }, { "V05",-3.243, 3.161, 4.321 }, { "V06",-1.485,-2.860, 2.153 },
+        { "V07",-3.001,-0.086,-6.081 }, { "V08", 2.412,-2.704,-0.215 }, { "V09", 3.461, 0.901,-3.155 }, { "V10",-0.204, 4.505,-2.072 } } ,
+      { { "C1",  2.178, 0.000, 1.206 }, { "C2",  0.733, 0.508, 1.236 }, { "C3",  0.000, 0.000, 0.000 },
+        { "C4",  0.728, 0.462,-1.271 }, { "C5",  2.172, 0.000,-1.203 }, { "O1",  2.874, 0.596, 2.298 },
+        { "O2",  0.088, 0.115, 2.439 }, { "O3", -1.357, 0.457,-0.031 }, { "O4",  0.635, 1.868,-1.478 }, { "O5", 2.817, 0.460, 0.003 } }
+    },
 
 // ./sails_minimax_exec -pdbin ~/Downloads/3wh1_refmac.pdb -mtzin ~/Downloads/3wh1_refmac.mtz -validation all -colin-fc "FWT,PHWT" -sugar "NAG"
 // ./sails_fingerprint_exec -mapin1 ~/Downloads/3wh1_refmac-NAG-4c1_max.map -mapin2 ~/Downloads/3wh1_refmac-NAG-4c1_min.map 
 //-pdbin ~/Downloads/3wh1_refmac-NAG-4c1.pdb -sugar "NAG" -pdbout ~/Downloads/3wh1_NAG_fp.pdb -mapradius 4 -maskradius 2
 	{
-		"NAG", "ligand", 16, 14, 14,
+		"NAG", "n-glycan", 16, 14, 14,
 		{
 { "PK1", 2.158, 0, 1.184 }, 
 { "PK1", 0.712, 0.451, 1.258 }, 
@@ -585,92 +627,92 @@ void sails::initialise_fingerprints () {
 //./sails_fingerprint_exec -mapin1 ~/Downloads/5o2x_refmac-MAN-4c1_max.map -mapin2 ~/Downloads/5o2x_refmac-MAN-4c1_min.map 
 //-pdbin ~/Downloads/5o2x_refmac-MAN-4c1.pdb -sugar "MAN" -pdbout ~/Downloads/5o2x_MAN_fp.pdb -mapradius 4 -maskradius 2
 
-	{
-		"MAN", "ligand", 12, 11, 11,  //number of atoms, pekas, voids
-		{
-{ "PK1", 2.207, 0, 1.213 }, 
-{ "PK1", 0.755, 0.454, 1.233 }, 
-{ "PK1", 0, 0, 0 }, 
-{ "PK1", 0.73, 0.461, -1.257 }, 
-{ "PK1", 2.193, 0, -1.205 }, 
-{ "PK1", 2.991, 0.555, -2.351 }, 
-{ "PK1", 0.711, 1.876, 1.363 }, 
-{ "PK1", -1.359, 0.481, 0.007 }, 
-{ "PK1", 0.167, -0.072, -2.442 }, 
-{ "PK1", 2.818, 0.44, 0.003 }, 
-{ "PK1", 2.236, -1.415, 1.325 } }, 
-{ { "VD1", -1, 1.5, -1.75 }, 
-{ "VD1", -1, 1, 2.25 }, 
-{ "VD1", -0.5, -1.25, 1.5 }, 
-{ "VD1", -1.25, -1.25, -1.25 }, 
-{ "VD1", 8.88178e-16, 2.25, -0.5 }, 
-{ "VD1", 1.25, -1.75, -0.5 }, 
-{ "VD1", -1.5, -1.25, 1 }, 
-{ "VD1", -1.25, 1.5, 1.75 }, 
-{ "VD1", -0.5, 2, -2 }, 
-{ "VD1", 1.75, 2.25, -0.5 }, 
-{ "VD1", 1, -0.5, 3 } }, 
-{ { "ATM", 2.207, 0, 1.213 }, 
-{ "ATM", 0.755, 0.454, 1.233 }, 
-{ "ATM", 0, 0, 0 }, 
-{ "ATM", 0.73, 0.461, -1.257 }, 
-{ "ATM", 2.193, 0, -1.205 }, 
-{ "ATM", 2.991, 0.555, -2.351 }, 
-{ "ATM", 0.711, 1.876, 1.363 }, 
-{ "ATM", -1.359, 0.481, 0.007 }, 
-{ "ATM", 0.167, -0.072, -2.442 }, 
-{ "ATM", 2.818, 0.44, 0.003 }, 
-{ "ATM", 2.993, 1.985, -2.382 }, 
-{ "ATM", 2.236, -1.415, 1.325 } }},
+ 	{
+ 		"MAN", "n-glycan", 12, 11, 11,  //number of atoms, pekas, voids
+ 		{
+ { "PK1", 2.207, 0, 1.213 }, 
+ { "PK1", 0.755, 0.454, 1.233 }, 
+ { "PK1", 0, 0, 0 }, 
+ { "PK1", 0.73, 0.461, -1.257 }, 
+ { "PK1", 2.193, 0, -1.205 }, 
+ { "PK1", 2.991, 0.555, -2.351 }, 
+ { "PK1", 0.711, 1.876, 1.363 }, 
+ { "PK1", -1.359, 0.481, 0.007 }, 
+ { "PK1", 0.167, -0.072, -2.442 }, 
+ { "PK1", 2.818, 0.44, 0.003 }, 
+ { "PK1", 2.236, -1.415, 1.325 } }, 
+ { { "VD1", -1, 1.5, -1.75 }, 
+ { "VD1", -1, 1, 2.25 }, 
+ { "VD1", -0.5, -1.25, 1.5 }, 
+ { "VD1", -1.25, -1.25, -1.25 }, 
+ { "VD1", 8.88178e-16, 2.25, -0.5 }, 
+ { "VD1", 1.25, -1.75, -0.5 }, 
+ { "VD1", -1.5, -1.25, 1 }, 
+ { "VD1", -1.25, 1.5, 1.75 }, 
+ { "VD1", -0.5, 2, -2 }, 
+ { "VD1", 1.75, 2.25, -0.5 }, 
+ { "VD1", 1, -0.5, 3 } }, 
+ { { "ATM", 2.207, 0, 1.213 }, 
+ { "ATM", 0.755, 0.454, 1.233 }, 
+ { "ATM", 0, 0, 0 }, 
+ { "ATM", 0.73, 0.461, -1.257 }, 
+ { "ATM", 2.193, 0, -1.205 }, 
+ { "ATM", 2.991, 0.555, -2.351 }, 
+ { "ATM", 0.711, 1.876, 1.363 }, 
+ { "ATM", -1.359, 0.481, 0.007 }, 
+ { "ATM", 0.167, -0.072, -2.442 }, 
+ { "ATM", 2.818, 0.44, 0.003 }, 
+ { "ATM", 2.993, 1.985, -2.382 }, 
+ { "ATM", 2.236, -1.415, 1.325 } }},
 
 
 
-//./sails_minimax_exec -pdbin ~/Downloads/5idb_refmac.pdb -mtzin ~/Downloads/5idb_refmac.mtz -validation all -colin-fc "FWT,PHWT" -sugar "BMA"
-//./sails_fingerprint_exec -mapin1 ~/Downloads/5idb_refmac-BMA-4c1_max.map -mapin2 ~/Downloads/5idb_refmac-BMA-4c1_min.map 
-//-pdbin ~/Downloads/5idb_refmac-BMA-4c1.pdb -sugar "BMA" -pdbout ~/Downloads/5idb_BMA_fp.pdb -mapradius 4 -maskradius 1
+// //./sails_minimax_exec -pdbin ~/Downloads/5idb_refmac.pdb -mtzin ~/Downloads/5idb_refmac.mtz -validation all -colin-fc "FWT,PHWT" -sugar "BMA"
+// //./sails_fingerprint_exec -mapin1 ~/Downloads/5idb_refmac-BMA-4c1_max.map -mapin2 ~/Downloads/5idb_refmac-BMA-4c1_min.map 
+// //-pdbin ~/Downloads/5idb_refmac-BMA-4c1.pdb -sugar "BMA" -pdbout ~/Downloads/5idb_BMA_fp.pdb -mapradius 4 -maskradius 1
 
-{
-		"BMA", "ligand", 13, 12, 12,
-		{
-{ "PK1", 2.899, 0.598, -2.331 }, 
-{ "PK1", 2.17, 0, -1.158 }, 
-{ "PK1", 0.728, 0.411, -1.235 }, 
-{ "PK1", 0, 0, 0 }, 
-{ "PK1", 0.748, 0.493, 1.237 }, 
-{ "PK1", 2.219, 0, 1.184 }, 
-{ "PK1", 2.763, 0.54, 0.023 }, 
-{ "PK1", 2.98, 0.402, 2.26 }, 
-{ "PK1", 0.694, 1.917, 1.316 }, 
-{ "PK1", -1.335, 0.523, -0.008 }, 
-{ "PK1", 0.18, -0.22, -2.394 }, 
-{ "PK1", 2.98, 0.402, 2.26 }}, 
-{{ "VD1", -1, 1.5, 1.5 }, 
-{ "VD1", 1.75, 2, 0 }, 
-{ "VD1", -0.75, 2, 0 }, 
-{ "VD1", 1.5, -1.25, 0 }, 
-{ "VD1", -1.5, -1.25, -0.5 }, 
-{ "VD1", 0.25, -1.5, 2.25 }, 
-{ "VD1", 1.5, 1.5, 2.75 }, 
-{ "VD1", -2.25, -0.25, 1.5 }, 
-{ "VD1", -1.25, 0.75, -2 }, 
-{ "VD1", 1, -1.5, -0.75 }, 
-{ "VD1", -0.75, 2, 2.25 }, 
-{ "VD1", -0.75, -0.75, 1.75 }}, 
-{{ "ATM", 2.899, 0.598, -2.331 }, 
-{ "ATM", 2.17, 0, -1.158 }, 
-{ "ATM", 0.728, 0.411, -1.235 }, 
-{ "ATM", 0, 0, 0 }, 
-{ "ATM", 0.748, 0.493, 1.237 }, 
-{ "ATM", 2.219, 0, 1.184 }, 
-{ "ATM", 2.763, 0.54, 0.023 }, 
-{ "ATM", 2.98, 0.402, 2.26 }, 
-{ "ATM", 0.694, 1.917, 1.316 }, 
-{ "ATM", -1.335, 0.523, -0.008 }, 
-{ "ATM", 0.18, -0.22, -2.394 }, 
-{ "ATM", 4.282, 0.242, -2.335 }, 
-{ "ATM", 2.98, 0.402, 2.26 }}} 
+ {
+ 		"BMA", "n-glycan", 13, 12, 12,
+ 		{
+ { "PK1", 2.899, 0.598, -2.331 }, 
+ { "PK1", 2.17, 0, -1.158 }, 
+ { "PK1", 0.728, 0.411, -1.235 }, 
+ { "PK1", 0, 0, 0 }, 
+ { "PK1", 0.748, 0.493, 1.237 }, 
+ { "PK1", 2.219, 0, 1.184 }, 
+ { "PK1", 2.763, 0.54, 0.023 }, 
+ { "PK1", 2.98, 0.402, 2.26 }, 
+ { "PK1", 0.694, 1.917, 1.316 }, 
+ { "PK1", -1.335, 0.523, -0.008 }, 
+ { "PK1", 0.18, -0.22, -2.394 }, 
+ { "PK1", 2.98, 0.402, 2.26 }}, 
+ {{ "VD1", -1, 1.5, 1.5 }, 
+ { "VD1", 1.75, 2, 0 }, 
+ { "VD1", -0.75, 2, 0 }, 
+ { "VD1", 1.5, -1.25, 0 }, 
+ { "VD1", -1.5, -1.25, -0.5 }, 
+ { "VD1", 0.25, -1.5, 2.25 }, 
+ { "VD1", 1.5, 1.5, 2.75 }, 
+ { "VD1", -2.25, -0.25, 1.5 }, 
+ { "VD1", -1.25, 0.75, -2 }, 
+ { "VD1", 1, -1.5, -0.75 }, 
+ { "VD1", -0.75, 2, 2.25 }, 
+ { "VD1", -0.75, -0.75, 1.75 }}, 
+ {{ "ATM", 2.899, 0.598, -2.331 }, 
+ { "ATM", 2.17, 0, -1.158 }, 
+ { "ATM", 0.728, 0.411, -1.235 }, 
+ { "ATM", 0, 0, 0 }, 
+ { "ATM", 0.748, 0.493, 1.237 }, 
+ { "ATM", 2.219, 0, 1.184 }, 
+ { "ATM", 2.763, 0.54, 0.023 }, 
+ { "ATM", 2.98, 0.402, 2.26 }, 
+ { "ATM", 0.694, 1.917, 1.316 }, 
+ { "ATM", -1.335, 0.523, -0.008 }, 
+ { "ATM", 0.18, -0.22, -2.394 }, 
+ { "ATM", 4.282, 0.242, -2.335 }, 
+ { "ATM", 2.98, 0.402, 2.26 }}} 
 
-   };
+    };
 }
 
 
